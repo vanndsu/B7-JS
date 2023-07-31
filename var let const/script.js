82f01542fb69268a846a96afbb5b1d8c4448456f
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-07-31 14:15:53 -0300

    Delete var let const directory

--- var let const/script.js
+++
@@ -1,9 +0,0 @@
-let nome2 = 'Vann'; var nome =  'Jão'; //Variável
-
-const nome3 = 'Kay'; //É uma "variável" constante, não pode ser alterada.
-
-console.log(' Nome 1:',nome,
-'\n',
-'Nome 2:',nome2,
-'\n',
-'Nome 3:',nome3);
