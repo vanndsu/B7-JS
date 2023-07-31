1ae4bbbec38ab62e62f864b01d48bd94e511a161
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-07-31 14:16:13 -0300

    Add files via upload

---
+++ Introdução/Inserindo na Pag/script.js
@@ -0,0 +1,13 @@
+alert('Olá Mundo!')
+console.log("Olá mundo 3.0!")
+
+let nome = "Vann"
+let idade = 22
+
+console.log(nome, idade)
+
+let lista = ["Ovo","Farinha","Oleo"]
+let nomeCompleto = {nome: "Francisco", sobrenome: "Ribeiro"}
+
+console.log(nomeCompleto.nome)
+console.log(typeof nome, typeof idade, typeof lista, typeof nomeCompleto)
