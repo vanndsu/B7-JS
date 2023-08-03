c37f32a212456ccef0b5fdf177df6a5855fb3339
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-08-03 14:20:51 -0300

    Add files via upload

---
+++ Exercicios/EX02/script.js
@@ -0,0 +1,27 @@
+//01
+
+let x =  10;
+let y = 5;
+console.log('THIS TRUE OR FALSE:',x > y);
+
+//02
+
+let w = '10';
+let z = 10;
+console.log('THIS TRUE OR FALSE:', w === z);
+
+//03
+
+let a = 10;
+let b = 5;
+console.log('THIS TRUE OR FALSE:', b < 10);
+
+//04
+
+let preco = 50.03;
+
+if(preco <= 45 && preco > 0){
+    console.log('TA BARATO!')
+}else if(preco > 45 && preco <=50){
+    console.log('TA BARATO MAS NEM TANTO...')
+}else console.log('TA CARO!')
