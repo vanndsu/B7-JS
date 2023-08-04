c43437e1c9b8dd5e78a744cca478451562cbc2ee
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-08-04 19:49:43 -0300

    Add files via upload

---
+++ Exercicios/EX01/01.js
@@ -0,0 +1,24 @@
+//1. Crie uma  variável para armazenar o nome
+// do carro e ponha o valor ferrari. E exiba-a.
+
+//2.  Crie uma variavel para armazenar o preço
+//do bolo, que custa R$ 19,99. E exiba-a.
+
+//3. Corrija o código a seguir (e exiba-o)
+// let "cidade" = São Paulo
+
+
+//1)
+
+let nomeCarro = "Ferrari"
+console.log("Nome do carro:",nomeCarro)
+
+//2)
+
+let precoBolo = 19.99
+console.log("Preço do bolo:",precoBolo)
+
+//3)
+
+let cidade = "São Paulo"
+console.log("Cidade:",cidade)
