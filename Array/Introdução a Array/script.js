13fee43c67eb5d6fa99a037bbf9e5512ef68e81c
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-08-15 18:05:43 -0300

    Add files via upload

---
+++ Array/Introdução a Array/script.js
@@ -0,0 +1,9 @@
+/*let colors = ['blue','green','vermelho'];
+
+console.log(colors[1],colors[2])*/
+
+let lista1 = [3, 5, 7];
+let lista2 = ["Vann", 65, lista1, ["xx", "yy", [1, 3]]];
+let lsitas = lista2;
+
+console.log(lsitas[3][2][1]);
