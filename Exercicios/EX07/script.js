ad3cd93f689fc975cd62edcea7b06f2d69027fe6
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-08-17 17:41:07 -0300

    Add files via upload

---
+++ Exercicios/EX07/script.js
@@ -0,0 +1,19 @@
+//1
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+console.log('1.', carros[1]);
+
+
+//2
+//carros.push('Audi')
+carros[1] = 'Audi'
+console.log('2. Lista com Audi')
+console.log(carros)
+
+//3
+console.log("3. Lista com Volvo");
+carros.push('Volvo')
+console.log(carros);
+
+//4
+console.log(`${carros.length} itens no array`);
+
