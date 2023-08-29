5733d779b374cecd2ba303e6f28df09371fd1efa
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-08-29 10:15:49 -0300

    Update script.js

--- Exercicios/EX08/script.js
+++ Exercicios/EX08/script.js
@@ -1,6 +1,12 @@
 let frutas = ['Maçã','Uva','Banana']
 
-for(let i = 0; i < frutas.length; i++){
+/*for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}*/
+/*for(let i of frutas){
+    console.log(i)
+}*/
+for(let i in frutas){
     console.log(frutas[i]);
 }
 
