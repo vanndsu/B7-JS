78cef589481196328792a9477ded8d9662d82b7e
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-09-05 18:08:45 -0300

    Update script.js

--- DOM/Introdução DOM/script.js
+++ DOM/Introdução DOM/script.js
@@ -1,3 +1,22 @@
+function clicou() {
+  const teste = document.querySelector(".teste");
+  const ul = teste.querySelector("ul");
+
+  /*ul.innerHTML = `
+  <li>Item Alterado</li>
+  <li>Item alterado2</li>
+  <li>Item alterado2</li>
+  `;*/ //alterando a UL toda
+
+  ul.children[0].innerHTML = "Alterado <strong>li 1</strong> "; //alterando somente um filho da UL (o li na posição 0)
+}
+
+let botao = document.querySelector(".bota1");
+
+botao.addEventListener("click", function () {
+  clicou();
+});
+
 /*let elementos = document.getElementsByTagName("h1");
 
 
@@ -5,27 +24,19 @@ document.querySelector('.teste')
 
 document.querySelector(".teste ul li")*/
 
-function clicou(){
-    console.log('Clicou');
+/*function clicou() {
+  console.log("Clicou");
+}*/
+
+/*
+function div() {
+  console.log("Aqui é uma div");
 }
 
-let botao = document.querySelector(".bota1")
+const clicar = () => {
+  console.log("Aqui é diferente!");
+};
 
-botao.addEventListener("click", function () {
-    clicou();
-});
-
-
-function div(){
-    console.log('Aqui é uma div')
-    
-}
-
-const clicar = () =>{
-    console.log('Aqui é diferente!');
-}
-
-document.querySelector(".buta2").addEventListener('click', () =>{
-    clicar();
-})
-
+document.querySelector(".buta2").addEventListener("click", () => {
+  clicar();
+});*/
