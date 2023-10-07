1c224d499fdece1d503debc0fedd7150bb74cea9
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-10-07 17:46:39 -0300

    Update script.js

--- Objetos e Orientação a Objetos/Introdução POO/script.js
+++ Objetos e Orientação a Objetos/Introdução POO/script.js
@@ -7,15 +7,6 @@ class Person {
   constructor(name) {
     this.name = name;
   }
-
-  takeAStep() {
-    this.steps = this.steps + 1; //pega os passos atuais do objeto e adiciona + 1;
-  }
-  setAge(newAge) {
-    if (typeof newAge == "number") {
-      this.age = newAge;
-    }
-  }
 }
 
 let p1 = new Person("João"); //instanciando um nome ao modelo feito no construtor
@@ -27,6 +18,3 @@ p1.age = 34; //alterando a instancia feita dentro da classe, somente para p1, o 
 console.log(`A Pessoa 1 tem o nome: ${p1.name}, e ${p1.age} anos`);
 console.log(`A Pessoa 2 tem o nome: ${p2.name}, e ${p2.age} anos`);
 console.log(`A Pessoa 2 tem o nome: ${p3.name}, e ${p3.age} anos`);
-
-p2.setAge(13); //setando um número na função para ser a nova idade de p2
-console.log(`Nova idade de ${p2.name}: ${p2.age} anos`);
