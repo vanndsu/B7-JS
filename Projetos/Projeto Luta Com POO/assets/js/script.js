c6baab109b545006df43bd4c229366e97551c762
vanndsu <99514321+vanndsu@users.noreply.github.com>
2023-10-16 17:48:22 -0300

    Update script.js

--- Projetos/Projeto Luta Com POO/assets/js/script.js
+++ Projetos/Projeto Luta Com POO/assets/js/script.js
@@ -1,13 +1,23 @@
 ///const { Knight, Sorcerer } = require("./classes.js"); //usado para  importar usando o CommomJS(Ambiente Node)
 
-import { Knight, Sorcerer } from "./classes.js";
+import {
+  Knight,
+  Sorcerer,
+  LittleMonster,
+  BigMonster,
+  Stage,
+} from "./classes.js";
 
-let k1 = new Knight("Guerreiro");
-let s1 = new Sorcerer("Mago");
+let knight = new Knight("Guerreiro");
+let sorcerer = new Sorcerer("Mago");
+let littleMonster = new LittleMonster();
+let bigMonster = new BigMonster();
 
-console.log(
-  `O ${k1.name} Tem uma vida máxima de ${k1.maxLife} e ${k1.defense} de defesa`
+const stage = new Stage(
+  knight,
+  littleMonster,
+  document.querySelector("#char"),
+  document.querySelector("#monster")
 );
-console.log(
-  `O ${s1.name} Tem uma vida máxima de ${s1.maxLife} e ${s1.defense} de defesa`
-);
+
+stage.start();
