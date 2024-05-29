cba0e3f1ad35cf49f51e50bc0048d4e88eb5aae8
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-05-29 16:56:11 -0400

    Add files via upload

--- projetoLutaFuncional/assets/functions.js
+++ projetoLutaFuncional/assets/functions.js
@@ -45,9 +45,40 @@ export const Stage = {
     this.fighter2El.querySelector("button").addEventListener("click", () => {
       this.doAttack(fighter2, fighter1);
     });
+    this.update(this.fighter1, this.fighter2, this.fighter1El, this.fighter2El);
+  },
 
+  doAttack(attacking, attacked) {
+    let attackFactor = (Math.random() * 2).toFixed(2);
+    let defenseFactor = (Math.random() * 2).toFixed(2);
+
+    let actualAttack = attacking.attack * attackFactor;
+    let actualDefense = attacked.defense * defenseFactor;
+
+    if (actualAttack > actualDefense && attacked.life > 0) {
+      attacked.life -= actualAttack;
+      
+      if (attacked.life < 0) {
+        attacked.life = 0;
+      }
+
+      console.log(
+        `${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${
+          attacked.name
+        }`
+      );
+    } else if (attacked.life <= 0) {
+      console.log(`${attacked.name} está morto`);
+    } else {
+      console.log(`${attacked.name} defendeu o ataque de ${attacking.name}`);
+    }
+
+    this.update(this.fighter1, this.fighter2, this.fighter1El, this.fighter2El);
+  },
+  update(fighter1, fighter2, fighter1El, fighter2El) {
     let pct1 = (fighter1.life / fighter1.maxLife) * 100;
     let pct2 = (fighter2.life / fighter2.maxLife) * 100;
+
     fighter1El.querySelector(".bar").style.width = `${pct1}%`;
     fighter2El.querySelector(".bar").style.width = `${pct2}%`;
 
@@ -58,36 +89,6 @@ export const Stage = {
     fighter2El.querySelector(".name").innerHTML = `${
       fighter2.name
     } - ${fighter2.life.toFixed(1)} HP`;
-  },
-  doAttack(attacking, attacked) {
-    if (attacked.life <= 0) {
-      console.log(`${attacked.name} está morto`);
-    }
-
-    let attackFactor = (Math.random() * 2).toFixed(2);
-    let defenseFactor = (Math.random() * 2).toFixed(2);
-
-    let actualAtack = attacking.attack * attackFactor;
-    let actualDefense = attacked.defense * defenseFactor;
-
-    attacking = actualAtack;
-    attacked = actualDefense;
-
-    if (actualAtack > actualDefense) {
-      let damage = actualAtack;
-
-      attacked.life = attacked.life - damage;
-
-      console.log(
-        `${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${
-          attacked.name
-        }`
-      );
-    }
-
-    console.log(
-      `${attacking.name} causou ${attacking.attack} de dano em ${attacked.name}`
-    );
   },
 };
 
