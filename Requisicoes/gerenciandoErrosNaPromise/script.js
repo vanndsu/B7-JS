0084f875fb3f6cc6048522e99dfe345e17fe3a8f
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-06-12 17:52:39 -0300

    Update script.js
    Adicionado o .status

--- Requisicoes/gerenciandoErrosNaPromise/script.js
+++ Requisicoes/gerenciandoErrosNaPromise/script.js
@@ -1,26 +1,22 @@
-const clicou = () =>{
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response)=>{
-        
-        return response.json();
-        
-        
+const clicou = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      //a função status retorna o código da operação.
+      console.log("Código da operação: " + response.status);
+      return response.json();
     })
-    .then((dados)=>{
-        alert('ID da requisição: '+dados[1].id)
-        
+    .then((dados) => {
+      alert("ID da requisição: " + dados[1].id);
     })
     //pega o que deu errado na requisição
-    .catch((error)=>{
-        alert('Erro ao executar a requisição')
-        console.log(error)
+    .catch((error) => {
+      alert("Erro ao executar a requisição");
+      console.log(error);
     })
     //callback que será executado no final de tudo
     //função backup
-    .finally(()=>{
-        alert('executado de intrometido')
-    })
-
-}
-document.querySelector('button').addEventListener('click',clicou)
-
+    .finally(() => {
+      alert("executado de intrometido");
+    });
+};
+document.querySelector("button").addEventListener("click", clicou);
