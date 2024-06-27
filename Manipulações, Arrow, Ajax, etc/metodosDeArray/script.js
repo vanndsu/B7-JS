2de838aaffc0b61c651958ea563d5ca8dfde178c
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-06-27 19:22:17 -0300

    Add files via upload

--- Manipulações, Arrow, Ajax, etc/metodosDeArray/script.js
+++ Manipulações, Arrow, Ajax, etc/metodosDeArray/script.js
@@ -2,6 +2,11 @@ let lista = ["ovo", "farinha", "corante", "massa"]; // Declara uma lista de ingr
 let listaDinamica = ["ovo", "farinha", "corante", "massa"]; // Declara outra lista de ingredientes para manipulação dinâmica
 let listaNumerica = [45, 78, 90, 54]; // Declara uma lista de números
 let listaNumerica2 = []; // Declara uma lista vazia para manipulação
+let listaUsuarios = [
+  { id: 1, nome: "Vanderson", sobrenome: "Ribeiro" },
+  { id: 2, nome: "Glau", sobrenome: "Pinheiro" },
+  { id: 3, nome: "Edvi", sobrenome: "Vania" },
+]; // Declara uma lista de objetos representando usuários
 
 let res = lista.toString(); // Converte a lista para uma string separada por vírgulas
 let res2 = lista.join("-"); // Converte a lista para uma string, com itens separados por "-"
@@ -68,32 +73,55 @@ for(let i in listaNumerica){
 console.log(listaNumerica2); // [90, 156, 180, 108]
 
 // Filtra os itens da listaNumerica que são menores que 90
-listaNumerica2 = listaNumerica.filter(function(item){
-    if(item < 90){
-        return true;
-    }else{
-        return false
-    }
-})
+listaNumerica2 = listaNumerica.filter(function (item) {
+  if (item < 90) {
+    return true;
+  } else {
+    return false;
+  }
+});
 
 console.log(listaNumerica2); // [45, 78, 54]
 
 // Verifica se todos os itens da listaNumerica são maiores que 100
-listaNumerica2 = listaNumerica.every((item) =>{
-    if(item > 100){
-        return true
-    }else{
-        return false
-    }
-})
+listaNumerica2 = listaNumerica.every((item) => {
+  if (item > 100) {
+    return true;
+  } else {
+    return false;
+  }
+});
 
 console.log(listaNumerica2); // false
 
 // Verifica se pelo menos um item da listaNumerica é maior que 40
-listaNumerica2 = listaNumerica.some((item) =>{
-
-    // Operador ternário: se o item for maior que 40, retorna true; caso contrário, retorna false
-    return (item > 40)? true : false
-})
+listaNumerica2 = listaNumerica.some((item) => {
+  // Operador ternário: se o item for maior que 40, retorna true; caso contrário, retorna false
+  return (item > 40) ? true : false;
+});
 
 console.log(listaNumerica2); // true
+
+// Encontra o primeiro item na listaNumerica que é igual a 90
+listaNumerica2 = listaNumerica.find((item) => {
+  return (item == 90) ? true : false;
+});
+console.log(listaNumerica2); // 90
+
+// Encontra o índice do primeiro item na listaNumerica que é igual a 90
+listaNumerica2 = listaNumerica.findIndex((item) => {
+  return (item == 90) ? true : false;
+});
+console.log(listaNumerica2); // 2
+
+// Encontra o primeiro usuário com sobrenome igual a "Vania"
+let pessoa = listaUsuarios.find((item) => {
+  return (item.sobrenome == "Vania") ? true : false;
+});
+console.log(pessoa); // { id: 3, nome: 'Edvi', sobrenome: 'Vania' }
+
+// Encontra o índice do primeiro usuário com sobrenome igual a "Vania"
+let pessoa2 = listaUsuarios.findIndex((item) =>
+  item.sobrenome == "Vania" ? true : false
+);
+console.log(pessoa2); // 2
