8b58e9a8ee3a74a5250736ecba56297564fb15de
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-06-27 13:47:32 -0300

    Add files via upload

--- Manipulações, Arrow, Ajax, etc/metodosDeArray/script.js
+++ Manipulações, Arrow, Ajax, etc/metodosDeArray/script.js
@@ -1,5 +1,7 @@
 let lista = ["ovo", "farinha", "corante", "massa"]; // Declara uma lista de ingredientes
-let listaDinamica = ["ovo", "farinha", "corante", "massa"]; // Declara uma outra lista de ingredientes para manipulação dinâmica
+let listaDinamica = ["ovo", "farinha", "corante", "massa"]; // Declara outra lista de ingredientes para manipulação dinâmica
+let listaNumerica = [45, 78, 90, 54]; // Declara uma lista de números
+let listaNumerica2 = []; // Declara uma lista vazia para manipulação
 
 let res = lista.toString(); // Converte a lista para uma string separada por vírgulas
 let res2 = lista.join("-"); // Converte a lista para uma string, com itens separados por "-"
@@ -52,3 +54,46 @@ console.log(lista); // ['corante', 'farinha', 'massa', 'ovo']
 // Inverte a ordem da lista
 lista.reverse();
 console.log(lista); // ['ovo', 'massa', 'farinha', 'corante']
+
+// Multiplica cada item da listaNumerica por 2 e retorna uma nova lista
+listaNumerica2 = listaNumerica.map(function (item) {
+  return item * 2;
+});
+
+/* Forma alternativa para o método acima.
+for(let i in listaNumerica){
+    listaNumerica2.push(listaNumerica[i]*2)
+}*/
+
+console.log(listaNumerica2); // [90, 156, 180, 108]
+
+// Filtra os itens da listaNumerica que são menores que 90
+listaNumerica2 = listaNumerica.filter(function(item){
+    if(item < 90){
+        return true;
+    }else{
+        return false
+    }
+})
+
+console.log(listaNumerica2); // [45, 78, 54]
+
+// Verifica se todos os itens da listaNumerica são maiores que 100
+listaNumerica2 = listaNumerica.every((item) =>{
+    if(item > 100){
+        return true
+    }else{
+        return false
+    }
+})
+
+console.log(listaNumerica2); // false
+
+// Verifica se pelo menos um item da listaNumerica é maior que 40
+listaNumerica2 = listaNumerica.some((item) =>{
+
+    // Operador ternário: se o item for maior que 40, retorna true; caso contrário, retorna false
+    return (item > 40)? true : false
+})
+
+console.log(listaNumerica2); // true
