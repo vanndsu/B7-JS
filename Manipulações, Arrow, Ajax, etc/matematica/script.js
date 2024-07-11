5d641c5557f8d4f1fc63d5c96e1e4c7109eb3bda
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-07-10 21:46:48 -0300

    Add files via upload

---
+++ Manipulações, Arrow, Ajax, etc/matematica/script.js
@@ -0,0 +1,18 @@
+let pi = Math.PI; // Atribui o valor de PI à variável 'pi'
+console.log(pi); // Imprime o valor de PI (3.141592653589793)
+
+let numerFloat = 60.90; // Declara um número de ponto flutuante
+let numerNeg = -60.9; // Declara um número negativo
+
+console.log(Math.pow(numerFloat, 2)); // Calcula 60.90 elevado à potência 2 e imprime o resultado
+console.log(Math.round(numerFloat)); // Arredonda 60.90 para o inteiro mais próximo (61)
+console.log(Math.floor(numerFloat)); // Arredonda 60.90 para baixo (60)
+console.log(Math.ceil(numerFloat)); // Arredonda 60.90 para cima (61)
+console.log(Math.abs(numerNeg)); // Retorna o valor absoluto de -60.9 (60.9)
+console.log(Math.min(6, 8, 9, 134, 7)); // Retorna o menor valor entre os números fornecidos (6)
+console.log(Math.max(6, 8, 9, 134, 7)); // Retorna o maior valor entre os números fornecidos (134)
+console.log(Math.random()); // Gera um número aleatório entre 0 (inclusivo) e 1 (exclusivo)
+console.log(Math.floor(Math.random() * 100)); // Gera um número aleatório entre 0 e 99 e arredonda para baixo
+console.log(Math.sin(numerFloat)); // Calcula o seno de 60.90 e imprime o resultado
+console.log(Math.cos(numerFloat)); // Calcula o cosseno de 60.90 e imprime o resultado
+console.log(Math.tan(numerFloat)); // Calcula a tangente de 60.90 e imprime o resultado
