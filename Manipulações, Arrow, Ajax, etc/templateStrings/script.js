cc223c76ff543afeac337d0809aa601d4224ae3d
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-07-18 21:05:16 -0300

    Add files via upload

---
+++ Manipulações, Arrow, Ajax, etc/templateStrings/script.js
@@ -0,0 +1,8 @@
+let nome = "Vandim"; // Declara a variável 'nome' e atribui o valor 'Vandim'
+let idade = 23; // Declara a variável 'idade' e atribui o valor 23
+
+// Usando template strings para criar uma frase
+frase = `Olá! Meu nome é ${nome}, tenho ${idade} anos e faço ${idade + 1} em setembro :)`;
+
+// Exibe a frase no console
+console.log(frase);
