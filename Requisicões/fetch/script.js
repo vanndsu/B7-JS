57ac4954babb09ba5bd0f44e2bda0f38802bb715
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-07-26 22:35:48 -0300

    Add files via upload

--- Requisicões/fetch/script.js
+++ Requisicões/fetch/script.js
@@ -1,27 +1,40 @@
 function loadPosts() {
-  document.getElementById("posts").innerHTML = "Carregando...";
-
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(function (resultado) {
-      return resultado.json();
-    })
-    .then(function (json) {
-      montarBlog(json);
-      alert('Aperte em "ok" para carregar os posts');
-    })
-    .catch(function (error) {
-      console.log("Deu problema!");
-    });
-}
-
-function montarBlog(lista) {
-  let html = "";
-
-  for (let i = 0; i < lista.length; i++) {
-    html += "<h3>" + lista[i].title + "</h3>";
-    html += lista[i].body + "</br>";
-    html += "<hr>";
+    // Define o texto "Carregando..." no elemento com o id "posts"
+    document.getElementById("posts").innerHTML = "Carregando...";
+  
+    // Faz uma requisição HTTP para a URL fornecida usando a função fetch
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(function (resultado) {
+        // Converte o resultado da resposta HTTP em formato JSON
+        return resultado.json();
+      })
+      .then(function (json) {
+        // Chama a função montarBlog, passando os dados JSON como argumento
+        montarBlog(json);
+  
+        // Exibe um alerta informando ao usuário para carregar os posts
+        alert('Aperte em "ok" para carregar os posts');
+      })
+      .catch(function (error) {
+        // Caso ocorra um erro durante a requisição, exibe uma mensagem no console
+        console.log("Deu problema!");
+      });
   }
-
-  document.getElementById("posts").innerHTML = html;
-}
+  
+  function montarBlog(lista) {
+    // Inicializa uma variável para armazenar o HTML gerado
+    let html = "";
+  
+    // Percorre a lista de posts recebidos
+    for (let i = 0; i < lista.length; i++) {
+      // Adiciona um título de post em um elemento <h3> e o corpo do post
+      html += "<h3>" + lista[i].title + "</h3>";
+      html += lista[i].body + "</br>";
+      // Adiciona uma linha horizontal para separar os posts
+      html += "<hr>";
+    }
+  
+    // Define o conteúdo HTML gerado no elemento com o id "posts"
+    document.getElementById("posts").innerHTML = html;
+  }
+  
