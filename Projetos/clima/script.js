7c292ccf569727e46aa5818e1958881b3a75317b
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-10-31 20:28:31 -0300

    Add files via upload

--- Projetos/clima/script.js
+++ Projetos/clima/script.js
@@ -4,27 +4,63 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
   event.preventDefault();
 
   let input = document.querySelector("#searchInput").value;
+  let lon;
+  let lat;
 
   if (input !== "") {
-    showWarning("Carregando...");
     let urlCoord = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURI(
       input
     )},BR&limit=1&appid=ceaec230071d85ac58ab752ddd07c9d8&units=metric&lang=pt_br`;
 
     let results = await fetch(urlCoord);
 
-    let json = await results.json();
+    let jsonLat = await results.json();
 
-    if (json) {
-      console.log(json[0].lon)
+    if (jsonLat) {
+      lon = jsonLat[0].lon;
+      lat = jsonLat[0].lat;
+      console.log(lon, lat);
+    } else {
+      showWarning("Não encontramos esta localização");
+    }
+
+    if (input !== "") {
+      let urlCiy = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ceaec230071d85ac58ab752ddd07c9d8`;
+
+      let resultsCity = await fetch(urlCiy);
+      let jsonCity = await resultsCity.json();
+
+      if (jsonCity) {
+        console.log(jsonCity);
+        showInfo({
+          name: jsonCity.name,
+          temp: jsonCity.main.temp,
+          tempIcon: jsonCity.weather[0].icon,
+          windSpeed: jsonCity.wind.speed,
+          windAngle: jsonCity.wind.deg,
+        });
+      }
     } else {
       showWarning("Não encontramos esta localização");
     }
   }
 });
 
-function showInfo() {
+function showInfo(json) {
+  let tempC = json.temp - 273.15;
   document.querySelector(".resultado").style.display = "block";
+  document.querySelector(".titulo").innerHTML = json.name;
+  document.querySelector(".tempInfo").innerHTML = tempC.toFixed(2);
+  document.querySelector(".ventoInfo").innerHTML = json.windSpeed;
+  document.querySelector(".ventoPonto").style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`;
+  document
+    .querySelector(".temp img")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
 }
 
 function showWarning(msg) {
