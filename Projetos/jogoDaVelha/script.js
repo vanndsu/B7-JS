b4b0e1e74616306500fc2533fd749b36c28dcf4a
vanndsu <99514321+vanndsu@users.noreply.github.com>
2024-11-05 20:57:00 -0300

    Add files via upload

---
+++ Projetos/jogoDaVelha/script.js
@@ -0,0 +1,51 @@
+//Initial Data
+let square = {
+  a1: "",
+  a2: "",
+  a3: "",
+  b1: "",
+  b2: "",
+  b3: "",
+  c1: "",
+  c2: "",
+  c3: "",
+};
+
+let player = "";
+let warning = "";
+let playing = false;
+
+reset();
+
+//Events
+document.querySelector(".reset").addEventListener("click", reset);
+
+//Functions
+function reset() {
+  warning = "";
+
+  let random = Math.floor(Math.random() * 2);
+
+  player = random === 0 ? "x" : "o";
+
+  for (let i in square) {
+    square[i] = "";
+  }
+
+  playing = true;
+
+  renderSquare();
+  renderInfo();
+}
+
+function renderSquare() {
+  for (let i in square) {
+    console.log("Item:", i);
+    let item = document.querySelector(`div[data-item=${i}]`);
+    item.innerHTML = square[i];
+  }
+}
+function renderInfo() {
+  document.querySelector(".vez").innerHTML = player;
+  document.querySelector(".resultado").innerHTML = warning;
+}
